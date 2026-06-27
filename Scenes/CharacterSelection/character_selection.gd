0dd7df5f404e110073fb94af50cde2b2ffcba84f
Caio De Souza Marinho <caide-so@c2r8p4.42sp.org.br>
2026-06-27 14:56:30 -0300

    add guard

--- Scenes/CharacterSelection/character_selection.gd
+++ Scenes/CharacterSelection/character_selection.gd
@@ -36,6 +36,9 @@ func load_selection_items() -> void:
 		card.set_data(data)
 
 func _on_play_button_pressed() -> void:
+	if not Global.selected_player and not Global.selected_weapon:
+		return
+	
 	ui_sound.play()
 	Transition.transition_to("res://Scenes/Arena/arena.tscn")
 
