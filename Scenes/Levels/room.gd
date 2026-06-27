45dbbf0b458f8a51e2b84e8b133c0ae09702b8d0
Lucas Gabriel Silva Xavier <lsilva-x@c1r4p2.42sp.org.br>
2026-06-27 08:12:26 -0300

    door closing in battles

--- Scenes/Levels/room.gd
+++ Scenes/Levels/room.gd
@@ -1,7 +1,6 @@
 extends Node2D
 
 class_name LevelRoom
-
 @onready var room_walls: Dictionary[Vector2i, TileMapLayer] = {
 	Vector2i.UP: %WallUp,
 	Vector2i.RIGHT: %WallRight,
@@ -9,9 +8,48 @@ class_name LevelRoom
 	Vector2i.LEFT: %WallLeft 
 }
 
+@onready var title_data: TileMapLayer = $TileData
+
+
+@onready var clear_door_nodes: Dictionary[Vector2i, TileMapLayer] = {
+	Vector2i.UP: %DoorUP,
+	Vector2i.RIGHT: %DoorRight,
+	Vector2i.DOWN: %DoorRight,
+	Vector2i.LEFT: %DoorLeft 
+}
+
+var titles: Array[Vector2i]
+var is_cleared: bool
+
 func _ready() -> void:
 	close_all_walls()
+	register_titles()
 	
+
+# ------------- DEBUGG --------
+#func _input(event: InputEvent) -> void:
+	#if event.is_action_pressed("ui_accept"):
+		#lock_room()
+	#if event.is_action_pressed("ui_cancel"):
+		#unlock_room()
+		
+func register_titles() -> void:
+	for title in title_data.get_used_cells():
+		titles.append(title)
+
+func lock_room() -> void:
+	for  direction in clear_door_nodes:
+		var wall_door = room_walls[direction]
+		var clear_door = clear_door_nodes[direction] 
+		
+		if wall_door and not wall_door.enabled:
+			clear_door.enabled = true
+			
+	
+func unlock_room() -> void:
+	for  direction in clear_door_nodes:
+		clear_door_nodes[direction].enabled = false
+		
 func open_wall(direction: Vector2i) -> void:
 	if room_walls.has(direction):
 		room_walls[direction].enabled = false
