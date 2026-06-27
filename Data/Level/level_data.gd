7b7a8a1b72942550177e6422670b23e69f2ce341
Lucas Gabriel Silva Xavier <lsilva-x@c1r4p2.42sp.org.br>
2026-06-27 05:28:57 -0300

    gen map

---
+++ Data/Level/level_data.gd
@@ -0,0 +1,26 @@
+extends Resource
+
+class_name LevelData
+
+# Qtd of sublevel (1-1, 1-2, 1-3, 1-4, 2-1, ....)
+@export var num_sub_levels := 4
+# Qtd of rooms per level
+@export var num_rooms := 10
+
+@export var room_size := Vector2i(384, 384)
+@export var room_scene: PackedScene
+
+@export var min_enemies_per_room := 5
+@export var max_enemies_per_room := 10
+
+# Scenes exports
+@export var h_corridor: PackedScene
+@export var v_corridor: PackedScene
+@export var corridor_size: Vector2i = Vector2i(192, 175)
+
+# Array of the enemies that will spawn in that room
+@export var enemy_scenes: Array[PackedScene]
+
+# Array of the items that will have a prob to drop in that room
+# Based on "level_store_data -> item_data(the item itself), item_prob(the probability of drop)
+@export var store_data: Array[LevelStoreData]
--- Scenes/Levels/room.gd
+++ Scenes/Levels/room.gd
@@ -1,3 +1,21 @@
 extends Node2D
 
-class_name Room
+class_name LevelRoom
+
+@onready var room_walls: Dictionary[Vector2i, TileMapLayer] = {
+	Vector2i.UP: %WallUp,
+	Vector2i.RIGHT: %WallRight,
+	Vector2i.DOWN: %WallDown,
+	Vector2i.LEFT: %WallLeft 
+}
+
+func _ready() -> void:
+	close_all_walls()
+	
+func open_wall(direction: Vector2i) -> void:
+	if room_walls.has(direction):
+		room_walls[direction].enabled = false
+
+func close_all_walls() -> void:
+	for key in room_walls:
+		room_walls[key].enabled = true
