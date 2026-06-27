947987f72d08cc3254eb6f7eba60b50ac7bbce7d
gxsilva <lssvgabriel@gmail.com>
2026-06-27 19:04:25 -0300

    final bullets

--- Autoloads/global.gd
+++ Autoloads/global.gd
@@ -3,6 +3,7 @@ extends Node
 var save_path = "user://save.json"
 
 const EXPLOSION_EFFECT_SCENE = preload("res://Scenes/Effects/explosion_effect.tscn")
+const DAMAGE_TEXT_SCENE = preload("res://Scenes/Extract/damage_text.tscn")
 
 const SPAWN_MARKER_SCENE = preload("res://Scenes/Effects/spawn_marker.tscn")
 const DEAD_PARTICLE_SCENE = preload("uid://dhg2agreu5c6d")
@@ -42,6 +43,13 @@ func create_explosion(pos: Vector2) -> void:
 	var explosion: Node2D =  EXPLOSION_EFFECT_SCENE.instantiate()
 	explosion.global_position = pos
 	get_tree().root.add_child(explosion)
+	
+func create_damage_text(value: float, pos: Vector2) -> void:
+	var damage: DamageText = DAMAGE_TEXT_SCENE.instantiate()
+	get_parent().add_child(damage)
+	var random_pos = randf_range(0, TAU)
+	damage.global_position = pos + Vector2.RIGHT.rotated(random_pos) * 20
+	damage.setup(value)
 	
 
 func save_data() -> void:
