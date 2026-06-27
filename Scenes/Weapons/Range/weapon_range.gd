83b764d43ce67d1bfde7db16e0e582d4124f4ee0
gxsilva <lssvgabriel@gmail.com>
2026-06-27 17:48:17 -0300

    bullet collision

--- Scenes/Weapons/Range/weapon_range.gd
+++ Scenes/Weapons/Range/weapon_range.gd
@@ -3,13 +3,21 @@ class_name WeaponRange
 
 @onready var sprite: Sprite2D = %Sprite2D
 @onready var fire_pos: Marker2D = %FirePos
+
 var direction: Vector2
+var cooldown: float
 
 func setup(data: WeaponData) -> void:
 	self.data = data
 
 func _process(delta: float) -> void:
 	rotate_weapon()
+	
+	cooldown -= delta
+	if Input.is_action_pressed("shoot"):
+		if cooldown <= 0:
+			use_weapon()
+			cooldown = data.cooldown
 
 func use_weapon() -> void:
 	var bullet: Bullet = data.bullet_scene.instantiate()
