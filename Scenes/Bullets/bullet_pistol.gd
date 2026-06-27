e3a3ee495153a5ed4fca1a51a2f39501b0ab8857
gxsilva <lssvgabriel@gmail.com>
2026-06-27 14:56:10 -0300

    WIP bullets

---
+++ Scenes/Bullets/bullet_pistol.gd
@@ -0,0 +1,20 @@
+extends Area2D
+class_name Bullet
+
+var data: WeaponData
+
+func setup(data: WeaponData) -> void:
+	self.data = data
+
+func _process(delta: float) -> void:
+	if not data: return
+	move_local_x(data.bullet_speed * delta)
+
+func _on_body_entered(body: Node2D) -> void:
+	#Global.create_explosion(global_position)
+	#
+	#if body is Enemy or body is Player:
+		#Global.create_damage_text(data.damage, body.global_position)
+		#body.health_component.take_damage(data.damage)
+	
+	queue_free()
